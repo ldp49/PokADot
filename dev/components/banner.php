6d9b6674cf1bc4c05e59583c0083197a2c4dd325
jtappen <ralfcazam@gmail.com>
2015-11-13 14:15:21 -0500

    initial commit

---
+++ dev/components/banner.php
@@ -0,0 +1,4 @@
+<!-- banner -->
+        <div id="banner">
+            <img id="logo" src="images/logo.png" alt="Pok-A-Dot logo">
+        </div><!-- banner div -->
---
+++ dev/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Pok-A-Dot</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <div id="container">
+        
+        <?php include "components/banner.php" ?>
+
+        <div class="menubar">
+            <a href="#">Home</a>
+            <a href="pages/page1.php">Page1</a>
+            <a href="pages/page2.php">Page2</a>
+            <a href="pages/about.php">About</a>
+        </div>
+    </div>
+</body>
+</html>
